2fc327c2843dfb7a5c551f8c23c3ce43b3ca53e9
wuxinabc <2669879798@qq.com>
2022-01-08 15:30:53 +0800

    '初始化'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // base:'/sxyblog/',
+    title: 'demo模板',
+    description: '我的第一个',
+    markdown: {
+        lineNumbers: true
+    },
+    theme: 'reco', // 使用皮肤
+    themeConfig: {
+        nav: [{ text: '主页', link: '/', icon: 'reco-home' }],
+        version: '1.0'
+    },
+    plugins: []
+}
