10146896f189e1f6622acdece66f00960b41d3d3
wuxinabc <2669879798@qq.com>
2022-01-09 14:12:10 +0800

    '全局使用vssue评论组件'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,8 +29,8 @@ module.exports = {
     //     ]
     // },
     themeConfig: {
-        lastUpdated:'最近更新',
-        nav:require('./nav'),
+        lastUpdated: '最近更新',
+        nav: require('./nav'),
         version: '1.0',
         // sidebar: [
         //     '/',
@@ -54,5 +54,16 @@ module.exports = {
         //     },
         // ],
     },
-    plugins: []
+    plugins: {
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github',
+            // 其他的 Vssue 配置
+            owner: 'wuxinabc',
+            repo: 'sxyblog',
+            clientId: 'f9368a69c836bd3cfcb2',
+            clientSecret: 'c4e7623d3e4649428252d19b62d1c28a8547ab59',
+            autoCreateIssue:true,
+        },
+    }
 }
---
+++ docs/.vuepress/theme/index.js
@@ -0,0 +1,4 @@
+//使用继承主题
+module.exports = {
+    extend: '@vuepress/theme-default'
+}
